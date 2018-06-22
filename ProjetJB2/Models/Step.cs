d2e2d637be2c2035bb738a6de236d71be612f3f2
IBN SALAH Yacine <yis75012@hotmail.com>
2018-06-22 12:38:03 +0200

    PROJET OK

---
+++ ProjetJB2/Models/Step.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace ProjetJB2.Models
+{
+    public class Step
+    {
+        public int Id { get; set; }
+        public String Description { get; set; }
+        public DateTime BeginDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public Nullable<int> ProjectId { get; set; }
+        public virtual Project Project { get; set; }
+    }
+}
